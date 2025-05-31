4a723a77412abbff9f734856a75abd4de1575fc4
Brian Dwyer <brian.dwyer@broadridge.com>
2025-05-31 14:42:46 -0400

    derp

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -2,8 +2,10 @@
 package utils
 
 import (
+	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 // GetEnv returns the value of the environment variable or fallback.
@@ -28,4 +30,34 @@ func SplitAndTrim(s string) []string {
 		}
 	}
 	return out
+}
+
+// ParseJiraTime tries multiple layouts to parse a time string for Jira worklog
+func ParseJiraTime(input string) (string, error) {
+	jiraFormat := "2006-01-02T15:04:05.000-0700"
+	layouts := []string{
+		jiraFormat,
+		time.RFC3339,
+		time.RFC3339Nano,
+		time.RFC1123,
+		time.RFC1123Z,
+		time.RFC822,
+		time.RFC822Z,
+		time.RFC850,
+		"2006-01-02T15:04:05-07:00",
+		"2006-01-02 15:04:05-07:00",
+		"2006-01-02 15:04:05Z07:00",
+		"2006-01-02T15:04:05",
+		"2006-01-02 15:04:05",
+		"2006-01-02",
+	}
+	var t time.Time
+	var err error
+	for _, layout := range layouts {
+		t, err = time.Parse(layout, input)
+		if err == nil {
+			return t.Format(jiraFormat), nil
+		}
+	}
+	return "", fmt.Errorf("could not parse time: %s", input)
 }
