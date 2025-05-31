0239a378a714291e0754342bbbe140a61f00d6a5
Brian Dwyer <brian.dwyer@broadridge.com>
2025-05-31 15:48:36 -0400

    Update

--- main.go
+++ main.go
@@ -3,16 +3,26 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"strings"
 
-	"mcp-atlassian-server/pkg/tools/confluence"
-	"mcp-atlassian-server/pkg/tools/jira"
+	log "github.com/sirupsen/logrus"
 
 	"github.com/mark3labs/mcp-go/mcp"
 	"github.com/mark3labs/mcp-go/server"
+
+	"mcp-atlassian-server/pkg/tools/confluence"
+	"mcp-atlassian-server/pkg/tools/jira"
 )
 
+func init() {
+	if os.Getenv("DEBUG") != "" {
+		log.SetLevel(log.DebugLevel)
+	}
+}
+
 func main() {
-	var hooks server.Hooks
+	hooks := &server.Hooks{}
 	hooks.AddBeforeAny(func(ctx context.Context, id any, method mcp.MCPMethod, message any) {
 
 	})
@@ -22,10 +32,20 @@ func main() {
 		"0.1.0",
 		server.WithToolCapabilities(true),
 		server.WithInstructions("Provides tools for interacting with Atlassian Jira."),
+		server.WithHooks(hooks),
 	)
 
-	confluence.AddTools(s)
-	jira.AddTools(s)
+	switch strings.ToUpper(os.Getenv("MCP_MODE")) {
+	case "JIRA":
+		jira.AddTools(s)
+	case "CONFLUENCE":
+		confluence.AddTools(s)
+	case "":
+		confluence.AddTools(s)
+		jira.AddTools(s)
+	default:
+		log.Fatal("Unknown MCP_MODE value")
+	}
 
 	if err := server.ServeStdio(s); err != nil {
 		fmt.Printf("Server error: %v\n", err)
